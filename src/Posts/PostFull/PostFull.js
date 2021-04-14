b8437de0ca773d6a3d03a693b3e25abf5fc9d8b9
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-04-14 15:40:33 +0600

    Stylized Postfull and ArticleFull

--- src/Posts/PostFull/PostFull.js
+++ src/Posts/PostFull/PostFull.js
@@ -1,8 +1,10 @@
+import classes from "./PostFull.module.css";
+
 const PostFull = ({ match }) => {
   console.log(match);
 
   return (
-  <h1>Post #{match.params.id}</h1>
+  <h1 className={classes.PostFull}>Post #{match.params.id}</h1>
   );
 }
 
--- src/components/Articles/ArticleFull/ArticleFull.js
+++ src/components/Articles/ArticleFull/ArticleFull.js
@@ -1,8 +1,10 @@
+import classes from "./ArticleFull.module.css";
+
 const ArticleFull = ({ match }) => {
     console.log(match);
   
     return (
-    <h1>Article #{match.params.id}</h1>
+    <h1 className={classes.ArticleFull}>Article #{match.params.id}</h1>
     );
   }
   
